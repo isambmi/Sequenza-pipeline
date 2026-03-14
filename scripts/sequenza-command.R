921fe4eccfe6fda1f8f44347fcba8989d98e6609
Isam Ibrahim <isam@cprosolutions.com.my>
2026-03-13 17:34:05 -1000

    Blocking PDF gen

--- scripts/sequenza-command.R
+++ scripts/sequenza-command.R
@@ -6,5 +6,24 @@ seqz.file <- args[2]
 
 seqz <- sequenza.extract(seqz.file, verbose = FALSE)
 cp.table <- sequenza.fit(seqz)
-sequenza.results(sequenza.extract = seqz,
+
+run_sequenza_no_plots <- function(seqz_obj, cp.table, sample.id, out.dir) {
+  old_pdf <- grDevices::pdf
+  
+  # Override pdf() locally
+  unlockBinding("pdf", as.environment("package:grDevices"))
+  assign("pdf", function(...) { invisible(NULL) }, envir = as.environment("package:grDevices"))
+  lockBinding("pdf", as.environment("package:grDevices"))
+  
+  on.exit({
+    unlockBinding("pdf", as.environment("package:grDevices"))
+    assign("pdf", old_pdf, envir = as.environment("package:grDevices"))
+    lockBinding("pdf", as.environment("package:grDevices"))
+  }, add = TRUE)
+  
+  sequenza.results(sequenza.extract = seqz_obj,
+    cp.table = cp.table, sample.id = sample.id)
+}
+
+run_sequenza_no_plots(seqz_obj = seqz,
     cp.table = cp.table, sample.id = sample.id)
