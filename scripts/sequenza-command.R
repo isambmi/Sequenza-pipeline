7167173990a3ba4ad4e11294483faa12c5a0408e
Isam Ibrahim <isambmi@hawaii.edu>
2026-03-16 21:05:42 -1000

    Fix script bugs

--- scripts/sequenza-command.R
+++ scripts/sequenza-command.R
@@ -1,11 +1,16 @@
 library(sequenza)
 
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 sample.id <- args[1]
 seqz.file <- args[2]
-num.threads <- args[3]
+num.threads <- suppressWarnings(as.integer(args[3]))
+
+if (is.na(num.threads) || num.threads < 1L) {
+  num.threads <- 1L
+}
 
 seqz <- sequenza.extract(seqz.file, verbose = FALSE, parallel = num.threads)
 cp.table <- sequenza.fit(seqz, mc.cores = num.threads)
 sequenza.results(sequenza.extract = seqz,
-    cp.table = cp.table, sample.id = sample.id)
+                 cp.table = cp.table,
+                 sample.id = sample.id)
